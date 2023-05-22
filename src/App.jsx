b65f5e177c59c4695460635cf08b5d467cfd825b
Azharnoushad <azharnoushad.com>
2023-05-22 06:40:02 +0530

    final commit------COMPLETED

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   const audioRef = useRef();
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? 'library-active' : ""}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
       <Song currentSong={currentSong} />
       <Player
